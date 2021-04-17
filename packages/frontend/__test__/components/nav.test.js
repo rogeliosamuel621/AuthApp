f085b574c17444aa0fe768a99fbdf7a24730c0e9
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-04-16 22:58:41 -0600

    add one more test, rename mock to mocks and move prettier config to .prettierrc

--- packages/frontend/__test__/components/nav.test.js
+++ packages/frontend/__test__/components/nav.test.js
@@ -7,17 +7,32 @@ import MockComponent from '../utils/MockComponent';
 describe('<Nav />', () => {
   test('Show drop dawn menu', () => {
     const navComponent = render(
-      <MockComponent
-        Component={<Nav authenticate={true} profilePic="none" />}
-      />
+      <MockComponent Component={<Nav authenticate={true} profilePic="none" />} />
     );
 
+    const nullDropDawnMenu = navComponent.queryByText('My profile')?.parentElement;
     const button = navComponent.getByAltText('arrow dawn image');
 
     fireEvent.click(button);
 
     const dropDawnMenu = navComponent.getByText('My profile').parentElement;
 
+    expect(nullDropDawnMenu).toBe(undefined);
     expect(dropDawnMenu).toBeInTheDocument();
+  });
+
+  test('Hide drop dawn menu', () => {
+    const navComponent = render(
+      <MockComponent Component={<Nav authenticate={true} profilePic="none" />} />
+    );
+
+    const button = navComponent.getByAltText('arrow dawn image');
+
+    fireEvent.click(button); // open
+    fireEvent.click(button); // close
+
+    const nullDropDawnMenu = navComponent.queryByText('My profile')?.parentElement;
+
+    expect(nullDropDawnMenu).toBe(undefined);
   });
 });
