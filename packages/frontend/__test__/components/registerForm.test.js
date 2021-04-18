c9b200f2fc4aecaedddce716a42e917cf2decd3d
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-04-17 18:48:31 -0600

    test(frontend): finish registerForm tests

--- packages/frontend/__test__/components/registerForm.test.js
+++ packages/frontend/__test__/components/registerForm.test.js
@@ -45,7 +45,6 @@ describe('<RegisterForm />', () => {
 
     const hasShowMessageClass = errorMessage.className.includes('ShowMessage');
 
-    screen.debug();
     fireEvent.click(errorMessage);
 
     const hasHideMessageClass = errorMessage.className.includes('HideMessage');
@@ -53,7 +52,59 @@ describe('<RegisterForm />', () => {
     expect(errorMessage).toBeInTheDocument();
     expect(hasShowMessageClass).toBe(true);
     expect(hasHideMessageClass).toBe(true);
+  });
 
-    screen.debug();
+  test('show & hide incorrect format password message', async () => {
+    require('dotenv').config();
+
+    const emailInput = screen.getByPlaceholderText('Enter your email');
+    const passwordInput = screen.getByPlaceholderText('Enter your password');
+    const registerButton = screen.getByText('Create an account');
+
+    fireEvent.change(emailInput, { target: { value: 'admin@gmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '123' } });
+
+    fireEvent.click(registerButton);
+
+    const errorMessage = screen.queryByText('Password must have at least 6 characters')
+      .parentElement;
+
+    const hasShowMessageClass = errorMessage.className.includes('ShowMessage');
+
+    fireEvent.click(errorMessage);
+
+    const hasHideMessageClass = errorMessage.className.includes('HideMessage');
+
+    expect(errorMessage).toBeInTheDocument();
+    expect(hasShowMessageClass).toBe(true);
+    expect(hasHideMessageClass).toBe(true);
+  });
+
+  test('show & hide email already taken message', async () => {
+    require('dotenv').config();
+
+    const emailInput = screen.getByPlaceholderText('Enter your email');
+    const passwordInput = screen.getByPlaceholderText('Enter your password');
+    const registerButton = screen.getByText('Create an account');
+
+    fireEvent.change(emailInput, { target: { value: 'sameEmail@gmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '123456' } });
+
+    fireEvent.click(registerButton);
+
+    let errorMessage;
+    await waitFor(() => {
+      errorMessage = screen.queryByText('Wrong credentials').parentElement; // this should be another msg
+    });
+
+    const hasShowMessageClass = errorMessage.className.includes('ShowMessage');
+
+    fireEvent.click(errorMessage);
+
+    const hasHideMessageClass = errorMessage.className.includes('HideMessage');
+
+    expect(errorMessage).toBeInTheDocument();
+    expect(hasShowMessageClass).toBe(true);
+    expect(hasHideMessageClass).toBe(true);
   });
 });
--- packages/frontend/__test__/utils/mocks/handlers.js
+++ packages/frontend/__test__/utils/mocks/handlers.js
@@ -3,6 +3,9 @@ import { Router } from 'express';
 const router = Router();
 
 router.post('/register', (req, res) => {
+  if (req.body.email === 'sameEmail@gmail.com') {
+    return res.status(401).send();
+  }
   return res.status(400).send();
 });
 
